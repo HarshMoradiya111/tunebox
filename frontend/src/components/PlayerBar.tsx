7904e4a863699b87ffeb3b4e4564671172ee9cbe
Harsh Moradiya <harshmoradiya111@gmail.com>
2026-07-25 23:39:59 +0530

    Set mobile player bar to full-width to prevent control button clipping

--- frontend/src/components/PlayerBar.tsx
+++ frontend/src/components/PlayerBar.tsx
@@ -189,7 +189,7 @@ export default function PlayerBar() {
         : Volume2;
 
   return (
-    <footer className="h-14 md:h-[90px] fixed md:static bottom-14 md:bottom-0 left-2 right-2 md:left-auto md:right-auto md:w-full rounded-lg md:rounded-none bg-[#282828] md:bg-black border border-[#3e3e3e] md:border-0 md:border-t md:border-[#282828] px-3 md:px-4 flex items-center justify-between z-40 select-none shadow-2xl transition-all">
+    <footer className="h-14 md:h-[90px] fixed md:static bottom-14 md:bottom-0 left-0 right-0 md:left-auto md:right-auto md:w-full bg-[#282828] md:bg-black border-t border-[#383838] md:border-[#282828] px-3 md:px-4 flex items-center justify-between z-40 select-none shadow-2xl transition-all overflow-hidden">
       {/* Mobile Top Progress Line */}
       {duration > 0 && (
         <div className="md:hidden absolute top-0 left-0 right-0 h-[2px] bg-[#282828]">
